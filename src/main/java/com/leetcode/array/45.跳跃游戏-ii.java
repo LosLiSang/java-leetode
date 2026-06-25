8032f704c434660712a0b45545d2e358a67a0329
Lisang <2458607905@qq.com>
2026-06-25 14:20:54 +0800

    feat: add 2 LeetCode array solutions (45. Jump Game II, 763. Partition Labels)

---
+++ src/main/java/com/leetcode/array/45.跳跃游戏-ii.java
@@ -0,0 +1,32 @@
+package com.leetcode.array;
+
+/*
+ * @lc app=leetcode.cn id=45 lang=java
+ *
+ * [45] 跳跃游戏 II
+ */
+
+// @lc code=start
+class Solution {
+    public int jump(int[] nums) {
+        int cur = 0, bestStep = 0, steps = 0, farthest = 0;
+        while(cur < nums.length - 1) {
+
+            if (cur + nums[cur] >= nums.length - 1) {
+                return steps + 1;
+            }
+
+            for(int i = cur; i <= cur + nums[cur]; i++) {
+                if (farthest < i + nums[i]) {
+                    bestStep = i;
+                    farthest = i + nums[i];
+                }
+            }
+            steps += 1;
+            cur = bestStep;
+        }
+        return steps;
+    }
+}
+// @lc code=end
+
---
+++ src/main/java/com/leetcode/array/763.划分字母区间.java
@@ -0,0 +1,34 @@
+package com.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=763 lang=java
+ *
+ * [763] 划分字母区间
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] appearIndex = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            appearIndex[c - 'a'] = i;
+        }
+        int start = 0, end = 0;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            end = Math.max(end, appearIndex[c - 'a']);
+            if (end == i) {
+                res.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
